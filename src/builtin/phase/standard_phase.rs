8be40d5243c9506dde64857c1f00fcd94603a23b
superbiebel <superbiebel@gmail.com>
2022-11-30 21:40:25 +0100

    base

--- src/builtin/phase/standard_phase.rs
+++ src/builtin/phase/standard_phase.rs
@@ -1,5 +1,6 @@
 use std::marker::PhantomData;
-use crate::interface::Phase;
+
+use crate::interface::{Phase, Score, StopType, Termination};
 
 struct StandardPhase<SolutionType, ScoreType, MoveChangeType, ScoreCalculatorType> {
     score_calculator: ScoreCalculatorType,
@@ -8,10 +9,22 @@ struct StandardPhase<SolutionType, ScoreType, MoveChangeType, ScoreCalculatorTyp
     phantom3: PhantomData<MoveChangeType>,
 }
 
-impl<SolutionType, ScoreType, MoveChangeType, ScoreCalculatorType, PhaseScopeType> Phase<SolutionType, PhaseScopeType> for StandardPhase<SolutionType, ScoreType, MoveChangeType, ScoreCalculatorType> {
-    fn start_solving(&mut self, solution: &mut SolutionType, phase_scope: PhaseScopeType) {
-
+impl<SolutionType, ScoreType, MoveChangeType, ScoreCalculatorType, TerminationType> Phase<SolutionType, StandardPhaseScope<SolutionType, ScoreType, TerminationType>> for StandardPhase<SolutionType, ScoreType, MoveChangeType, ScoreCalculatorType>
+where TerminationType: Termination<SolutionType, ScoreType> {
+    fn start_solving<'a>(&mut self, solution: &mut SolutionType, phase_scope: StandardPhaseScope<SolutionType, ScoreType, TerminationType>) {
+        let termination = &phase_scope.termination;
+        let last_score:Option<ScoreType> = None;
+        loop {
+            match termination.should_stop(&last_score, solution) {
+                StopType::StopSolver => {}
+                StopType::StopPhase => {break}
+                StopType::StopPhaseAndSolver => {break}
+            }
+        }
     }
 }
-pub struct StandardPhaseScope {
+pub struct StandardPhaseScope<SolutionType, ScoreType, TerminationType> where TerminationType: Termination<SolutionType, ScoreType> {
+    pub(crate) termination: TerminationType,
+    pub(crate) phantom: PhantomData<SolutionType>,
+    pub(crate) phantom2: PhantomData<ScoreType>
 }
--- src/builtin/solver/standard_solver.rs
+++ src/builtin/solver/standard_solver.rs
@@ -1,27 +1,36 @@
+use std::marker::PhantomData;
+
 use crate::builtin::phase::standard_phase::StandardPhaseScope;
+use crate::builtin::termination::time_termination::{TerminationLevel, TimeTermination};
 use crate::interface::{Phase, Solver, StopType, Termination};
+
 struct StandardSolver<SolutionType, TerminationType, ScoreType> where TerminationType: Termination<SolutionType, ScoreType> {
-    phases: Vec<Box<dyn Phase<SolutionType, StandardPhaseScope>>>,
+    phases: Vec<Box<dyn Phase<SolutionType, StandardPhaseScope<SolutionType, ScoreType, TerminationType>>>>,
     termination: TerminationType,
-    _phantom_score: ScoreType
+    _phantom_score: PhantomData<ScoreType>,
 }
 impl<SolutionType, TerminationType, ScoreType> Solver<SolutionType> for StandardSolver<SolutionType, TerminationType, ScoreType>
     where TerminationType: Termination<SolutionType, ScoreType> {
     fn start_solving(&mut self, solution: &mut SolutionType) {
-        if !self.termination.solver_started(solution) {
-            return;
-        }
+
         for phase in &mut self.phases {
-            if !self.termination.phase_started(solution) {
-                return
-            }
-            phase.start_solving(solution,  StandardPhaseScope{});
+
+                if !TerminationType::phase_started(&mut self.termination, solution) {
+                    return
+                }
+
+
+                phase.start_solving(solution, StandardPhaseScope {
+                    termination: self.termination,
+                    phantom: Default::default(),
+                    phantom2: Default::default(),
+                });
+
             match self.termination.should_stop(&None, solution) {
                 StopType::StopSolver => {return}
                 StopType::StopPhase => {}
                 StopType::StopPhaseAndSolver => {return}
             }
         }
-
     }
 }
